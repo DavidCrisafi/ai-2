68436ad5d85b58b9dcd38c34f538c2f294fce3bd
David Crisafi-Lurtsema <crisafilurtsema@gmail.com>
2019-09-30 12:27:44 -0800

    More initial stuff
    New format for main stock list and a couple important functions

--- stock-calc.py
+++ stock-calc.py
@@ -1,10 +1,29 @@
 import sys
 
+def stockSwap(tfbStocks, fromStockId, toStockId):
+	amount = tfbStocks[fromStockId] * 0.1
+	tfbStocks[fromStockId] -= amount
+	tfbStocks[toStockId] += amount
+
+def calculateReturns(tfbStocks, allStocks):
+	returns = []
+	for stocks in tfbStocks:
+		test = 1 #Ignore this line, it's a placeholder
+		
+
 stockFileName = sys.argv[1]
 stockFile = open(stockFileName, 'r')
 stocks = []
 index = -1
 
+COMPANY = 0
+PRICE = 1
+CHANGE = 2
+
+#Creates a 2d list of all stocks and assigns it to the stocks variable. Here's how the list is formatted:
+#	stock['initial'] accesses the data for a specific company (replace 'initial' with the company's initial). From there,
+#	stock['initial'][price] accesses the company's stock price.
+#	stock['initial'][change] accesses the company's percent change over 30 days.
 for row in stockFile:
 	#Skip over the header
 	if (index == -1):
@@ -12,9 +31,12 @@ for row in stockFile:
 		continue
 		
 	separatedData = row.split(',')
-	separatedData[1] = float(separatedData[1])
-	separatedData[2] = float(separatedData[2].rstrip('\n'))
-	stocks.insert(index, separatedData)
+	separatedData[PRICE] = float(separatedData[1])
+	separatedData[CHANGE] = float(separatedData[2].rstrip('\n'))
+	data = []
+	data.insert("price", separatedData[PRICE])
+	data.insert("change", separatedData[CHANGE])
+	stocks.insert(separatedData[COMPANY], data)
 	index += 1
 
 print(stocks)
